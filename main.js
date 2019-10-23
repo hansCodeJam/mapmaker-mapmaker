b3eb71a2ad9498a63d08b0876add31814257ab2f
Hans Vos <hans.vos@codeimmersives.com>
2019-10-23 10:31:20 -0400

    few changes

--- main.js
+++ main.js
@@ -1,28 +1,85 @@
-function doubleAll(str) {
+function doubleAll(arr) {
+  let newArray = [];
 
+  for(let i = 0; i < arr.length; i++){
+    newArray.push(arr[i] * 2)
+  }
+  return newArray;
 }
 
-function absoluteValues(str) {
+function absoluteValues(arr) {
+  let newArray = [];
 
+  for(let i = 0; i < arr.length; i++) {
+    if(arr[i] === arr[i]){
+      newArray.push(arr[i]);
+    } else if(arr[i] < 0){
+      newArray.push(arr[i] * -1)
+    }
+  }
+  return newArray;
 }
 
-function yelledGreetings(str) {
+function yelledGreetings(arr) {
+  let newArray = [];
 
+  for(let i = 0; i < arr.length; i++) {
+    newArray.push(arr[i] + "!")
+  }
+  return newArray
 }
 
-function changeToInitials(str) {
 
+function changeToInitials(arr) {
+  let newArray = [];
+
+  for(let i = 0; i < arr.length; i++) {
+    const currentInitials = arr[i];
+    const firstIndex = currentInitials[0];
+    // const lastIndex = 
+    // const lastIndex = currentInitials[0].length - 1;
+    newArray.push(firstIndex)
+  }
+
+  return newArray;
 }
 
-function doubleOdd(str) {
 
+function doubleOdd(arr) {
+  let newArray = [];
+
+  for(let i = 0; i < arr.length; i++){
+    if((arr[i] % 2) !== 1){
+      newArray.push(arr[i])
+    } else {
+      newArray.push(arr[i] * 2)
+    }
+  }
+
+  return newArray;
 }
 
-function upperCaseFirstLetters(str) {
+function upperCaseFirstLetters(arr) {
+  let newArray = [];
 
+  for(let i = 0; i < arr.length; i++){
+    const currentWord = arr[i];
+    const firstInitials = currentWord[0];
+    const restOfWord = currentWord.slice(1, currentWord.length);
+    newArray.push(firstInitials.toUpperCase() + restOfWord.toLowerCase())
+    }
+
+  return newArray;
 }
 
-function add1ToLeft(str) {
+
+function add1ToLeft(arr) {
+  let newArray = [];
+
+  for(let i = 0; i < arr.length; i++){
+    const toString = arr[i].toString();
+    const 
+  }
 
 }
 
